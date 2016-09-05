2dee40cc56240940af8cdebd67a804621cd74606
Thomas Conte <thomas@conte.com>
2016-09-05 17:08:03 +0200

    Fix for Truffle 2.0

--- list-transactions.js
+++ list-transactions.js
@@ -2,20 +2,28 @@ var fossilizer = Fossilizer.deployed();
 var emailEvent = fossilizer.EmailFossilized({}, {fromBlock: 0});
 var docEvent = fossilizer.DocumentFossilized({}, {fromBlock: 0});
 
-emailEvent.get(function(error, logs) {
-  if (error) return;
-  for (var i=0; i<logs.length; i++) {
-    console.dir(logs[i]);
-    var date = new Date(logs[i].args.timestamp.c);
-    console.log(date);
-  }
-});
+module.exports = function(callback) {
+  emailEvent.get(function(error, logs) {
+    if (error) {
+      console.log(error);
+      return;
+    }
+    for (var i=0; i<logs.length; i++) {
+      console.dir(logs[i]);
+      //var date = new Date(logs[i].args.timestamp.c);
+      //console.log(date);
+    }
+  });
 
-docEvent.get(function(error, logs) {
-  if (error) return;
-  for (var i=0; i<logs.length; i++) {
-    console.dir(logs[i]);
-    var date = new Date(logs[i].args.timestamp.c);
-    console.log(date);
-  }
-});
+  docEvent.get(function(error, logs) {
+    if (error) {
+      console.log(error);
+      return;
+    }
+    for (var i=0; i<logs.length; i++) {
+      console.dir(logs[i]);
+      //var date = new Date(logs[i].args.timestamp.c);
+      //console.log(date);
+    }
+  });
+}
